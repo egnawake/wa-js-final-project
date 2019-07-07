d963b9f440a700c9609ea14916b8b4ab21147169
João Inácio <joao.crvs.inacio@gmail.com>
2019-07-07 16:57:45 +0100

    Improve feedback display content

--- js/number-guessing.js
+++ js/number-guessing.js
@@ -15,6 +15,7 @@ function init() {
 
   const goal = randomInt(0, 20);
   let attempts = 0;
+  dom.feedback.textContent = 'Try to find the secret number (between 0 and 20)!';
 
   function getGuess() {
     return Number(dom.guessInput.value);
@@ -29,7 +30,7 @@ function init() {
     } else if (guess > goal) {
       dom.feedback.textContent = `${guess} is larger than the secret number.`;
     } else {
-      dom.feedback.textContent = `Congratulations! The secret number was ${guess}!`;
+      dom.feedback.textContent = `Congratulations! The secret number was ${guess}! Number of attempts: ${attempts}`;
       dom.guessInput.setAttribute('disabled', '');
       dom.guessButton.setAttribute('disabled', '');
     }
