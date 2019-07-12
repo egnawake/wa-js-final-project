d29cf7d70a85e95a18a7465482647b7708445947
João Inácio <joao.crvs.inacio@gmail.com>
2019-07-12 22:14:03 +0100

    Rebuild largest number with clearer UX

--- js/largest-number.js
+++ js/largest-number.js
@@ -2,28 +2,43 @@ function largest(a, b) {
   return a > b ? a : b;
 }
 
+function isNumber(value) {
+  return !isNaN(Number(value));
+}
+
+function isInputValid(input) {
+  return input !== '' && isNumber(input);
+}
+
 function init() {
   const dom = {
     firstNumber: document.getElementById('first-number'),
-    secondNumber: document.getElementById('second-number'),
-    result: document.getElementById('result')
+    secondNumber: document.getElementById('second-number')
   }
 
-  dom.firstNumber.value = 0;
-  dom.secondNumber.value = 0;
-
   function displayLargestNumber() {
-    const firstNumberInput = dom.firstNumber.value;
-    const secondNumberInput = dom.secondNumber.value;
+    const firstInput = dom.firstNumber.value;
+    const secondInput = dom.secondNumber.value;
 
-    let result;
-    if (firstNumberInput === '' || secondNumberInput === '') {
-      result = 'Please insert two numbers.';
+
+    if (isInputValid(firstInput) && isInputValid(secondInput)) {
+      const firstNumber = Number(firstInput);
+      const secondNumber = Number(secondInput);
+
+      if (firstNumber > secondNumber) {
+        dom.firstNumber.className = 'largest';
+        dom.secondNumber.className = '';
+      } else if (secondNumber > firstNumber) {
+        dom.secondNumber.className = 'largest';
+        dom.firstNumber.className = '';
+      } else {
+        dom.firstNumber.className = 'equal';
+        dom.secondNumber.className = 'equal';
+      }
     } else {
-      result = largest(Number(firstNumberInput), Number(secondNumberInput));
+      dom.firstNumber.className = 'invalid'
+      dom.secondNumber.className = 'invalid';
     }
-
-    dom.result.textContent = result;
   }
 
   dom.firstNumber.addEventListener('input', displayLargestNumber);
